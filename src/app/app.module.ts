9cd9ff0cff0eba9ef083c241b31c08fe9706f519
kien <kien.nguyentrung1699@gmail.com>
2023-09-17 20:19:23 +0700

    generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,12 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SignupComponent } from './components/signup/signup.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { WorkerListComponent } from './components/worker-list/worker-list.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LoginComponent, SignupComponent],
+  declarations: [AppComponent, HomeComponent, LoginComponent, SignupComponent, HeaderComponent, FooterComponent, WorkerListComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
