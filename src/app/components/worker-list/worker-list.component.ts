2552817a372fc7974512bf89261a14bb9d6a258f
Tran Gia Bao <baozach123@gmai.com>
2023-09-19 19:39:17 +0700

    render worker list from DB

--- src/app/components/worker-list/worker-list.component.ts
+++ src/app/components/worker-list/worker-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WorkerServiceService } from 'src/app/services/worker-service.service';
+import { Worker } from 'src/app/common/worker';
 
 @Component({
   selector: 'app-worker-list',
   templateUrl: './worker-list.component.html',
-  styleUrls: ['./worker-list.component.scss']
+  styleUrls: ['./worker-list.component.scss'],
 })
-export class WorkerListComponent {
+export class WorkerListComponent implements OnInit {
+  workers: Worker[] = [];
 
+  constructor(private workerService: WorkerServiceService) {}
+
+  ngOnInit(): void {
+    this.workerService.getAllWorkers().subscribe((data: any) => {
+      this.workers = data.accounts;
+    });
+  }
 }
